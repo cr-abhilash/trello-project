8fdda0c94fd4f544dc67a0a67c2a2dfb4f47605b
Abhilash CR <abhilash.cr@mountblue.tech>
2020-09-14 11:57:16 +0530

    implemented logic for mounting Boards on UI

--- src/body/Body.js
+++ src/body/Body.js
@@ -3,11 +3,34 @@ import PermIdentityOutlinedIcon from '@material-ui/icons/PermIdentityOutlined';
 import AccessTimeOutlinedIcon from '@material-ui/icons/AccessTimeOutlined';
 import {Link } from 'react-router-dom'
 import './body.css'
-const Body = ()=>{
+function Body({data,match}) {
+    console.log(match)
+    const Boards=data.map(({id,name,prefs})=>{
+        return <Link to={`/${match.Path}/`}><div key={id} className="board1"
+        style={
+            {backgroundImage:(prefs.backgroundImage)?`url(${prefs.backgroundImage})`:'none',
+            backgroundColor:prefs.backgroundColor
+        }}>
+        {name}
+    </div>
+    </Link>
+    })
     return (
         <div className="body" >
-         Body
+        {/* <AccessTimeOutlinedIcon size="small"/>
+        <strong>Personal Boards</strong> */}
+        <div className="title">
+        <PermIdentityOutlinedIcon size="small"/>
+        <strong>Personal Boards</strong>
+        </div>
+        <div className="boards">
+           {Boards}
+           <a><div className="board1 newBoard">
+               Create new board 
+           </div></a>
+        </div>
         </div>
     )
 }
+
 export default Body
