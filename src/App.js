16a8b81aed2db8d640a9092345bef89fe962ed0b
Abhilash CR <abhilash.cr@mountblue.tech>
2020-09-14 11:30:24 +0530

    installed required packages and basic setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
+import React ,{Component}from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import Header from './Header/Header';
+import SideBar2 from './sidebar/sideBar2';
+import Body from './body/Body'
+import Home from './body/Home'
+import {BrowserRouter as Router, Route,Link,Switch} from 'react-router-dom'
+class  App extends Component{
+  constructor(props){
+    super(props)
+  
+    this.state = {
+       boards:[]
+    }
+  }
+  
+  
+  render(){
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
     </div>
+    </Router>
   );
+}
+
 }
 
 export default App;
